dc39be7b5814bf189b1a5bee96f621cc334e0052
Alejandro <sav7972@rit.edu>
2020-10-09 11:34:11 -0400

    Fixed pick up

--- Player.h
+++ Player.h
@@ -20,9 +20,7 @@ class Player {
     static const int memorySize = 100;
     int IDmemory[memorySize];
     string name;
-
-    // Set to be 20
-    int seatCredit = 20;
+    int seatCredit;
     Seat seatAssignment;
 
   public:
